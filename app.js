332ed6552875a451b38924da980feb0ccd7de94b
Shivank Pathak <shivank.pathak@quantiphi.com>
2020-04-08 17:07:28 +0530

    Initialize and create the environment

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true},() =>{
+    console.log("Connected to DB");
+});
+
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+app.get('/', (req, res) =>{
+    res.send("Welcome to ticket booking system!");
+});
+
+app.listen(process.env.PORT);
